cca17d1ce3e177e012df0cb3831b0ffdede02879
Waga Odongo <wagaodongo@gmail.com>
2019-03-20 08:28:13 +0300

    user model
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import { makeExecutableSchema } from "graphql-tools";
 import typeDefs from "./schema";
 import resolvers from "./resolvers";
 
+import models from "./models";
+
 const schema = makeExecutableSchema({
 	typeDefs,
 	resolvers
--- models/index.js
+++ models/index.js
@@ -1,19 +1,21 @@
 import Sequelize from "sequelize";
-const sequelize = new Sequelize(
-	config.database,
-	config.username,
-	config.password
-);
+const sequelize = new Sequelize("slack", "postgres", "postgres");
 
 const models = {
-	user: sequelize.import("./users")
+	user: sequelize.import("./users"),
+	channel: sequelize.import("./channel"),
+	member: sequelize.import("./member"),
+	message: sequelize.import("./message"),
+	team: sequelize.import("./team")
 };
 
-Object.keys(db).forEach((modelName) => {
-	if ("associate" in db[modelName]) {
-		db[modelName].associate(db);
+Object.keys(models).forEach((modelName) => {
+	if ("associate" in models[modelName]) {
+		models[modelName].associate(models);
 	}
 });
 
 models.sequelize = sequelize;
 models.Sequelize = Sequelize;
+
+export default models;
